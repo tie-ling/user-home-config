d016c33fe457e3d23deebee3f79832a47e227c57
Yan Cao <yc@example.com>
2025-01-01 00:18:54 +0100

    reorganize gitignore
    Signed-off-by: Yan Cao <yc@example.com>

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -44,7 +44,7 @@
      (:name "archived" :query
             "not tag:deleted not tag:inbox  not tag:sent")))
   (notmuch-show-logo nil)
-  (org-agenda-files '("~/Documents/wbesichtigung.org"))
+  (org-agenda-files nil)
   (read-buffer-completion-ignore-case t)
   (ring-bell-function nil)
   (scroll-bar-mode nil)
@@ -144,3 +144,15 @@
 
 
 (use-package text-mode)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(org-babel-load-languages '((haskell . t) (python . t) (emacs-lisp . t))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
