88e32ee00faf05b83471e0641f599e3386c83ce2
Yan Cao <yc@example.com>
2025-02-07 19:49:29 +0100

    smaller foot font

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -299,3 +299,16 @@ char."
               "%Y-%m-%d %a "
               (time-add (current-time) (* 24 3600)))
              "\\1:00>"))))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(auctex ledger-mode counsel pyim-basedict notmuch nix-mode magit haskell-ts-mode)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
