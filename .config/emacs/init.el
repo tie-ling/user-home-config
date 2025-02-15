6df4c9ec6901277e2d485e64ef8536e1f6158e14
Yan Cao <yc@example.com>
2025-02-15 21:48:54 +0100

    use thinkbook
    Signed-off-by: Yan Cao <yc@example.com>

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -241,3 +241,16 @@ char."
                ("\\mathbb{C}" . ?ℂ)
                ("\\mathbb{K}" . ?𝕂)))
       (add-to-list 'tex--prettify-symbols-alist symb))))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(auctex sml-mode pyim-basedict notmuch nix-mode magit ledger-mode haskell-ts-mode counsel)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
