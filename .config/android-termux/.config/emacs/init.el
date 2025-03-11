485747157a020f6c5643255d03d1e4b0adc9c36f
Yan Cao <yc@example.com>
2025-03-11 10:25:03 +0100

    add android termux
    Signed-off-by: Yan Cao <yc@example.com>

---
+++ .config/android-termux/.config/emacs/init.el
@@ -0,0 +1,98 @@
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+(package-initialize)
+
+(use-package emacs
+  :custom
+  (xterm-mouse-mode nil)
+  (auto-fill-function 'do-auto-fill t)
+  (calendar-week-start-day 1)
+  (custom-enabled-themes '(modus-vivendi) nil nil)
+  (display-battery-mode nil)
+  (display-time-mode nil)
+  (electric-pair-mode t)
+  (enable-local-variables nil)
+  (indent-tabs-mode nil)
+  (inhibit-startup-screen t)
+  (mail-envelope-from 'header)
+  (mail-specify-envelope-from t)
+  (menu-bar-mode nil)
+  (message-default-mail-headers "Reply-To: Yuchen Guo <yc@apvc.uk>")
+  (message-sendmail-envelope-from 'header)
+  (mode-line-compact 'long)
+  (modus-themes-bold-constructs nil)
+  (modus-themes-inhibit-reload nil)
+  (modus-themes-italic-constructs t)
+  (modus-themes-mixed-fonts t)
+  (network-security-level 'paranoid)
+  (require-final-newline t)
+  (send-mail-function 'sendmail-send-it)
+  (sendmail-program "msmtp")
+  (tab-always-indent 'complete)
+  (tool-bar-mode nil)
+  (user-mail-address "yc@apvc.uk"))
+
+(use-package vertico
+  :ensure t
+  :custom
+  (vertico-mode t))
+
+(use-package magit
+  :ensure t)
+
+(use-package org
+  :bind
+  (("C-c a" . org-agenda))
+  :custom
+  (org-agenda-inhibit-startup t)
+  (org-agenda-window-setup 'only-window)
+  (org-agenda-start-with-log-mode nil)
+  (org-clock-mode-line-total 'current)
+  (org-agenda-prefix-format
+   '((agenda . " %i %?-12t% s")
+     (todo . " %i %-12:c")
+     (tags . " %i %-12:c")
+     (search . " %i %-12:c")))
+  (org-agenda-files '("~/Projects/orgmode/agenda/logbook/"
+                      "~/Projects/orgmode/agenda/schedule/"
+                      "~/Projects/orgmode/agenda/todo/"))
+  (org-directory '("~/Documents/org"))
+  (org-agenda-span 'day)
+  (org-display-custom-times nil)
+  (org-time-stamp-custom-formats '("%m-%d" . "%H:%M"))
+  (org-latex-compiler "lualatex")
+  (org-export-initial-scope 'buffer)
+  (org-modules
+   '(ol-bbdb ol-bibtex ol-doi ol-eww ol-info ol-irc ol-mhe ol-rmail org-tempo))
+  (org-structure-template-alist
+   '(("a" . "export ascii")
+     ("c" . "center")
+     ("C" . "comment")
+     ("e" . "example")
+     ("E" . "export")
+     ("h" . "export html")
+     ("l" . "export latex")
+     ("q" . "quote")
+     ("s" . "src")
+     ("v" . "verse")
+     ("py" . "src python"))))
+
+(use-package ledger-mode
+  :ensure t
+  :custom
+  ((ledger-report-use-strict t)
+   (ledger-default-date-format "%y-%m-%d"))
+  :mode ("\\.ledger\\'"))
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
