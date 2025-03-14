7c43286a5e4dd8b26eac2e29f8f9a8bdbe793d7f
Yan Cao <yc@example.com>
2025-03-14 18:57:34 +0100

    use real emacs on android

--- .config/android-termux/.config/emacs/init.el
+++ .config/android-termux/.config/emacs/init.el
@@ -1,7 +1,3 @@
-(require 'package)
-(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
-(package-initialize)
-
 (use-package emacs
   :custom
   (auto-fill-function 'do-auto-fill t)
@@ -84,23 +80,3 @@
      ("s" . "src")
      ("v" . "verse")
      ("py" . "src python"))))
-
-(use-package ledger-mode
-  :ensure t
-  :custom
-  ((ledger-report-use-strict t)
-   (ledger-default-date-format "%y-%m-%d"))
-  :mode ("\\.ledger\\'"))
-
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages nil))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
