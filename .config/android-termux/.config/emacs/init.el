5352fe637feb93ec53fe6f0a7dc2638de9ca3f17
Your Name <you@example.com>
2025-03-14 20:31:58 +0100

    enable menu bar
    Signed-off-by: Your Name <you@example.com>

--- .config/android-termux/.config/emacs/init.el
+++ .config/android-termux/.config/emacs/init.el
@@ -1,4 +1,8 @@
 (custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
  '(default ((t (:height 180 :family "Droid Sans Mono")))))
 
 (use-package emacs
@@ -24,7 +28,7 @@
   (inhibit-startup-screen t)
   (mail-envelope-from 'header)
   (mail-specify-envelope-from t)
-  (menu-bar-mode nil)
+  (menu-bar-mode t)
   (message-default-mail-headers "Reply-To: Yuchen Guo <yc@apvc.uk>")
   (message-sendmail-envelope-from 'header)
   (mode-line-compact 'long)
@@ -39,7 +43,7 @@
   (send-mail-function 'sendmail-send-it)
   (sendmail-program "msmtp")
   (tab-always-indent 'complete)
-  (tool-bar-mode nil)
+  (tool-bar-mode t)
   (user-mail-address "yc@apvc.uk")
   (xterm-mouse-mode nil))
 
@@ -100,3 +104,9 @@
      ("s" . "src")
      ("v" . "verse")
      ("py" . "src python"))))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages nil))
