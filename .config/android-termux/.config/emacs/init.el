9340f904d61ff6d0ecbd59aec5e9fef294aa3837
Your Name <you@example.com>
2025-03-15 15:46:37 +0100

    default height 120

--- .config/android-termux/.config/emacs/init.el
+++ .config/android-termux/.config/emacs/init.el
@@ -3,7 +3,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:height 220 :family "JuliaMono"))))
+ '(default ((t (:height 120 :family "JuliaMono"))))
  '(menu ((t (:height 1))))
  '(mode-line ((t (:height 1)))))
 
