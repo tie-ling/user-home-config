9b9308b49c18e142bf751d7c65a6f1fe1f490391
Your Name <you@example.com>
2025-03-31 20:17:31 +0200

    fix and emacs font height

--- .config/android-termux/.config/emacs/init.el
+++ .config/android-termux/.config/emacs/init.el
@@ -3,7 +3,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:height 120 :family "JuliaMono"))))
+ '(default ((t (:family "JuliaMono"))))
  '(menu ((t (:height 0.75))))
  '(mode-line ((t (:height 0.75)))))
 
