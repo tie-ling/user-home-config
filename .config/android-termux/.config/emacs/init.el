914588a15c325cf7df3155608cab0a708f863d62
Yan Cao <yc@example.com>
2025-05-12 18:45:27 +0200

    update

--- .config/android-termux/.config/emacs/init.el
+++ .config/android-termux/.config/emacs/init.el
@@ -3,9 +3,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:family "JuliaMono"))))
- '(menu ((t (:height 0.75))))
- '(mode-line ((t (:height 0.75)))))
+ '(default ((t (:family "JuliaMono")))))
 
 (use-package emacs
   :bind
