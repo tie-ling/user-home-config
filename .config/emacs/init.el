3abf1e74775c871f1ebbf99b3a75df55ec613dd2
Yan Cao <yc@example.com>
2025-10-03 02:13:03 +0200

    yt-dlp workaround
    Signed-off-by: Yan Cao <yc@example.com>

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -288,3 +288,9 @@ char."
      ("Sans Serif" "helv" "helvetica" "arial" "fixed")
      ("helv" "helvetica" "arial" "fixed")))
  '(package-selected-packages nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
