4cdcb8644728f9f9daf0739d8f852a3165fbadce
Yan Cao <yc@example.com>
2025-12-20 12:36:07 +0100

    update
    Signed-off-by: Yan Cao <yc@example.com>

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -267,3 +267,15 @@ char."
 
 (use-package slob-dict
   :load-path "lisp")
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
